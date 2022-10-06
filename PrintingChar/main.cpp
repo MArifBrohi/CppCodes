44d7d1639517b99e26b3b4747dc8134cdd5d8792
Hi there :) Here is Aarif <105107942+MArifBrohi@users.noreply.github.com>
2022-10-06 17:54:49 +0500

    Create main.cpp

---
+++ PrintingChar/main.cpp
@@ -0,0 +1,23 @@
+/* Online C++ Compiler and Editor */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  
+    cout<<"Enter any character: ";
+    char ch;
+   cin>>ch;
+    if(ch>='a' && ch<='z')
+    {
+        cout<<"it is a character: ";
+    }
+    else{
+        cout<<"not a character";
+    }
+  
+
+   
+   return 0;
+}
