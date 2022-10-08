d57b44d2c0c9072d1515c5dba100c1739b447843
Hi there :) Here is Aarif <105107942+MArifBrohi@users.noreply.github.com>
2022-10-08 18:36:17 +0500

    Update main.java

--- Task_05/main.java
+++ Task_05/main.java
@@ -24,7 +24,7 @@ int main()
     cin>>currentBalance;
      
     
-    if(accountType=='s' || accountType == 'S')
+    if(accountType=='s' || accountType == 'S')    // if your account is saving then this condition will run
     {
         cout << "Account Number: " << accountNumber << endl;
         cout << "Account Type: Savings" << endl;
@@ -42,22 +42,22 @@ int main()
                 << " at " << savingInterest*100 << "%" << endl;
             }
     }
-    else if(accountType=='c' || accountType == 'C')
+    else if(accountType=='c' || accountType == 'C')     // if upper condition fails then else if condition will execute
     {
             cout << "Account Number: " << accountNumber << endl;
             cout << "Account Type: Checking" << endl;
             cout << "Minimum Balance: $" << minimumBalance << endl;
             cout << "Current Balance: $" << currentBalance << endl;
 
-            if (currentBalance < minimumBalance)
+            if (currentBalance < minimumBalance)            // here will your curr and min balance will be checked 
             {
                 cout << "Service Fee: $" << checking_Service_Charge << endl;
             } 
 
-            else if (currentBalance <= (minimumBalance + 5000.00)) 
+            else if (currentBalance <= (minimumBalance + 5000.00))      // if your cur balance is not less then mim balance, this will execute
             {
-                cout << "Interest Earned: $" << currentBalance * checkingInterest
-                << " at " << checkingInterest*100 << "%" << endl;
+                cout << "Interest Earned: $" << currentBalance * checkingInterest       // your cur balance will be multiplied with 0.03;
+                << " at " << checkingInterest*100 << "%" << endl;                   
             }
     }
     else
